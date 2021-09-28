fcb79aac397f607b92594863a8465684c6f47664
ssu20 <>
2021-09-28 15:24:55 +0900

    0928

---
+++ js/jQurey.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+
+    /** 메인 **/
+
+    $('.bxslider').bxSlider({
+        auto: true,
+        speed: 500,
+        pause: 4000,
+        made: 'fade',
+        autoControls: true,
+        pager:true,
+    });
+
+});
